8d016c08056d5f5e982ef89892373fbf5a69c8c9
NampallyHarshini <121306410+NampallyHarshini@users.noreply.github.com>
2023-05-31 07:40:38 +0530

    Create configkeeper.py

---
+++ lpkit/configkeeper.py
@@ -0,0 +1,15 @@
+from os.path import join as osPathJoin
+from flask import current_app
+
+
+class ConfigKeeper:
+    def get(name:str):
+        return current_app.config[name]
+    
+    def asInt(name:str):
+        return int(ConfigKeeper.get(name))
+    
+    def getReportFolderPath():
+        return osPathJoin(current_app.root_path, ConfigKeeper.get('REPORT_CSVS_RELATIVE_PATH'))
+
+
